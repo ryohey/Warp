6c2e9f4bfcb846b40d5222021e1300adbbc5f01b
ryohey <info@codingcafe.jp>
2022-01-30 15:46:24 +0900

    Implement Encoder

--- Assets/Scripts/Runtime/Encoder/Encoder.cs
+++ Assets/Scripts/Runtime/Encoder/Encoder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 using UnityEngine;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
@@ -28,27 +29,45 @@ namespace Warp
         Dictionary<string, string> Attributes { get; }
     }
 
-    public struct GameObjectElement
-    {
-        public Dictionary<string, ValueOrReference<object>> attributes;
-        public IList<ValueOrReference<ComponentElement>> components;
-    }
+    public interface IElementRef { }
 
-    public struct GameObjectElementRef
+    public struct GameObjectElementRef : IElementRef
     {
-        public Dictionary<string, ValueOrReference<object>> attributes;
+        public IDictionary<string, ValueOrReference<object>> attributes;
         public IList<Reference> components;
     }
 
-    public struct ComponentElement
+    public struct TransformElementRef : IElementRef
     {
-        public Dictionary<string, ValueOrReference<object>> attributes;
+        public IDictionary<string, ValueOrReference<object>> attributes;
+        public IList<Reference> children;
+    }
+
+    public struct ComponentElementRef : IElementRef
+    {
+        public IDictionary<string, ValueOrReference<object>> attributes;
+    }
+
+    public interface IElement { }
+
+    [Serializable]
+    public struct GameObjectElement : IElement
+    {
+        public Dictionary<string, object> attributes;
+        public List<ComponentElement> components;
+        public List<GameObjectElement> children;
+    }
+
+    [Serializable]
+    public struct ComponentElement : IElement
+    {
+        public Dictionary<string, object> attributes;
     }
 
     public struct ValueOrReference<T>
     {
         public T value;
-        public Reference reference;
+        public Reference? reference;
     }
 
     public struct Reference
@@ -70,7 +89,7 @@ namespace Warp
             var documents = SplitPrefabDocument(text);
             Debug.Log(string.Join("=======\n", documents));
 
-            var objects = new List<Referenceable<object>>();
+            var objects = new List<Referenceable<IElementRef>>();
 
             foreach (var chunk in documents)
             {
@@ -78,14 +97,105 @@ namespace Warp
                 {
                     objects.Add(ParseGameObjectDocument(chunk));
                 }
+                else if (chunk.objectClassType == 4)
+                {
+                    objects.Add(ParseTransformDocument(chunk));
+                }
+                else
+                {
+                    objects.Add(ParseComponentDocument(chunk));
+                }
             }
+
+            var gameObjectElement = ToGameObjectElement(objects);
+            string json = JsonConvert.SerializeObject(gameObjectElement);
+
+            Debug.Log(json);
+        }
+
+        static GameObjectElement ToGameObjectElement(List<Referenceable<IElementRef>> objects)
+        {
+            IElementRef ResolveReference(Reference reff)
+            {
+                if (reff.fileID == "0")
+                {
+                    return null;
+                }
+                var obj = objects.Where(obj => obj.fileID == reff.fileID)
+                    .Cast<Referenceable<IElementRef>?>()
+                    .FirstOrDefault();
+                if (obj.HasValue)
+                {
+                    return obj.Value.value;
+                }
+                throw new Exception("Cannot resolve reference for fileID: " + reff.fileID);
+            }
+
+            Dictionary<string, object> ResolveAttributes(IDictionary<string, ValueOrReference<object>> attributes)
+            {
+                return attributes.ToDictionary(
+                    entry => entry.Key,
+                    entry => entry.Value.reference.HasValue ? ResolveReference(entry.Value.reference.Value) : entry.Value.value
+                    );
+            }
+
+            GameObjectElement CreateGameObjectElement(TransformElementRef transform)
+            {
+                var gameObject = objects
+                    .Where(obj => obj.value is GameObjectElementRef && obj.fileID == transform.attributes["m_GameObject"].reference.Value.fileID)
+                    .Cast<Referenceable<IElementRef>?>()
+                    .FirstOrDefault();
+
+                if (!gameObject.HasValue)
+                {
+                    throw new Exception("GameObject not found");
+                }
+
+                var go = (GameObjectElementRef)(gameObject.Value.value);
+
+                return new GameObjectElement
+                {
+                    attributes = ResolveAttributes(go.attributes),
+                    children = transform.children.Select(t =>
+                    {
+                        var elm = (TransformElementRef)ResolveReference(t);
+                        return new GameObjectElement
+                        {
+                            attributes = ResolveAttributes(elm.attributes),
+                            children = elm.children.Select(ResolveReference).Cast<GameObjectElement>().ToList()
+                        };
+                    }).ToList()
+                };
+            }
+
+            var rootTransform = objects
+                .Where(obj =>
+            {
+                if (obj.value is TransformElementRef elm)
+                {
+                    if (elm.attributes["m_Father"].reference.Value.fileID == "0")
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            })
+                .Cast<Referenceable<IElementRef>?>()
+                .FirstOrDefault();
+
+            if (!rootTransform.HasValue)
+            {
+                throw new Exception("Root Transform not found");
+            }
+
+            return CreateGameObjectElement((TransformElementRef)(rootTransform.Value.value));
         }
 
         static ValueOrReference<object> ParseAttribute(object attr)
         {
-            if (attr is Dictionary<string, object>)
+            if (attr is Dictionary<object, object>)
             {
-                var obj = attr as Dictionary<string, object>;
+                var obj = attr as Dictionary<object, object>;
 
                 if (obj.ContainsKey("fileID"))
                 {
@@ -105,19 +215,18 @@ namespace Warp
             };
         }
 
-        static Referenceable<object> ParseGameObjectDocument(YamlChunk chunk)
+        static Referenceable<IElementRef> ParseGameObjectDocument(YamlChunk chunk)
         {
-            // component: {fileID: 8345145045156654008}
             var deserializer = new DeserializerBuilder().Build();
             var parsed = deserializer.Deserialize<IDictionary<string, IDictionary<string, object>>>(chunk.document);
             var attributes = parsed.Values.First();
             var components = attributes["m_Component"] as IList<object>;
             attributes.Remove("m_Component");
 
-            return new Referenceable<object>
+            return new Referenceable<IElementRef>
             {
                 fileID = chunk.fileID,
-                value = new GameObjectElement
+                value = new GameObjectElementRef
                 {
                     attributes = attributes
                     .ToDictionary(
@@ -125,17 +234,64 @@ namespace Warp
                                 (entry) => ParseAttribute(entry.Value)),
                     components = components.Select(comp =>
                     {
+                        // component: {fileID: 8345145045156654008}
                         var c = comp as IDictionary<object, object>;
                         var value = c["component"] as Dictionary<object, object>;
-                        return new ValueOrReference<ComponentElement>
+                        return new Reference
                         {
-                            reference = new Reference
-                            {
-                                fileID = value["fileID"] as string
-                            }
+                            fileID = value["fileID"] as string
                         };
                     })
                     .ToList()
+                }
+            };
+        }
+
+        static Referenceable<IElementRef> ParseTransformDocument(YamlChunk chunk)
+        {
+            var deserializer = new DeserializerBuilder().Build();
+            var parsed = deserializer.Deserialize<IDictionary<string, IDictionary<string, object>>>(chunk.document);
+            var attributes = parsed.Values.First();
+            var children = attributes["m_Children"] as IList<object>;
+            attributes.Remove("m_Children");
+
+            return new Referenceable<IElementRef>
+            {
+                fileID = chunk.fileID,
+                value = new TransformElementRef
+                {
+                    attributes = attributes
+                    .ToDictionary(
+                        (entry) => entry.Key,
+                                (entry) => ParseAttribute(entry.Value)),
+                    children = children.Select(comp =>
+                    {
+                        var c = comp as IDictionary<object, object>;
+                        return new Reference
+                        {
+                            fileID = c["fileID"] as string
+                        };
+                    })
+                    .ToList()
+                }
+            };
+        }
+
+        static Referenceable<IElementRef> ParseComponentDocument(YamlChunk chunk)
+        {
+            var deserializer = new DeserializerBuilder().Build();
+            var parsed = deserializer.Deserialize<IDictionary<string, IDictionary<string, object>>>(chunk.document);
+            var attributes = parsed.Values.First();
+
+            return new Referenceable<IElementRef>
+            {
+                fileID = chunk.fileID,
+                value = new ComponentElementRef
+                {
+                    attributes = attributes
+                    .ToDictionary(
+                        (entry) => entry.Key,
+                                (entry) => ParseAttribute(entry.Value)),
                 }
             };
         }
